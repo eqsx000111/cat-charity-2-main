2c6b85098d190e930f9b274308581d16fa134392
Ivan Ilnickiy <deddotu@yandex.ru>
2026-01-12 17:18:39 +0300

    edit CheckConstraint

--- app/models/base.py
+++ app/models/base.py
@@ -22,7 +22,7 @@ class InvestableBase(Base):
             name='check_full_amount_positive'
         ),
         CheckConstraint(
-            'invested_amount >= 0 AND invested_amount <= full_amount',
+            '0 <= invested_amount <= full_amount',
             name='check_invested_amount_range'
         ),
     )
